409857624278eeb33d5c80ef6fd8909468e916e6
Vlad Dincă <dincavlad01@gmail.com>
2025-12-10 08:05:05 +0200

    Refactor to use class

--- javascript.js
+++ javascript.js
@@ -2,13 +2,23 @@
 let myLibrary = [];
 let ids = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = crypto.randomUUID();
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+//   this.id = crypto.randomUUID();
+// }
+class Book{
+  constructor(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+  }
 }
+
 
 function addBookToLibrary(title, author, pages, read) {
 
